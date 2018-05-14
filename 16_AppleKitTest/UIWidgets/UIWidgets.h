7d20b725039a4e0d68de69c469cd8da49faa799c
daoquan <daoquanlu@gmail.com>
2018-05-14 22:24:45 +0800

    finished cloudkit demo

---
+++ 16_AppleKitTest/UIWidgets/UIWidgets.h
@@ -0,0 +1,21 @@
+//
+//  UIWidgets.h
+//  UIWidgets
+//
+//  Created by daoquan on 2018/5/14.
+//  Copyright © 2018年 deer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIWidgets.
+FOUNDATION_EXPORT double UIWidgetsVersionNumber;
+
+//! Project version string for UIWidgets.
+FOUNDATION_EXPORT const unsigned char UIWidgetsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIWidgets/PublicHeader.h>
+#import <UIWidgets/StarRatingControl.h>
+#import <UIWidgets/BadgeView.h>
+#import <UIWidgets/CheckedButton.h>
+
